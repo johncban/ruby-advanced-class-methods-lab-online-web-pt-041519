7ecf7023960b30da0b35f9f54c36c9fe5e9a3c97
johncban <johncban@gmail.com>
2019-05-19 06:08:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,9 +15,11 @@ class Song
     song
   end
 
-  def self.create_by_name(song_name)
+  def self.new_by_name(name)
+
+  def self.create_by_name(name)
     song = self.new
-    song.name = song_name
+    song.name = name
     song.save
     song
   end
